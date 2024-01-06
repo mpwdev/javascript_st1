5bbda0a9b7da299c6815e8a4e23694c64c0d70c6
MP Web Developer <mprwdev@gmail.com>
2024-01-06 22:50:57 +0200

    async code intro

---
+++ async-01/app.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('button');
+const output = document.querySelector('p');
+
+function trackUserHandler() {
+  console.log('Clicked!');
+}
+
+button.addEventListener('click', trackUserHandler);
+
+// ----
+
+let result = 0;
+
+for (let i = 0; i < 100000000; i++) {
+  // result = result + i;
+  result += i;
+}
+
+console.log(result);
