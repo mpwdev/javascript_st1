20793e512100a7991d02447811e9aa212eae8d3c
MP Web Developer <mprwdev@gmail.com>
2024-01-07 20:36:44 +0200

    example rewrite function setTimeout with promise

--- async-01/app.js
+++ async-01/app.js
@@ -1,22 +1,37 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
   // console.log('Clicked!');
   navigator.geolocation.getCurrentPosition(
     (posData) => {
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
+      // setTimeout(() => {
+      //   console.log(posData);
+      // }, 2000);
+      setTimer(2000).then((data) => {
+        console.log(data, posData);
+      });
     },
     (errData) => {
       console.log(errData);
     }
   );
 
-  setTimeout(() => {
-    console.log('Timer done! /zero timout/');
-  }, 0);
+  // setTimeout(() => {
+  //   console.log('Timer done! /zero timout/');
+  // }, 0);
+  setTimer(1000).then(() => {
+    console.log('Timer done! /timeout 1000/');
+  });
 
   console.log('Getting position.../out of navigator/');
 }
