76cd5691be41d1afa3046bc136a204f735f04d4f
MP Web Developer <mprwdev@gmail.com>
2024-01-07 20:24:01 +0200

    examples of async code

--- async-01/app.js
+++ async-01/app.js
@@ -2,18 +2,34 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 function trackUserHandler() {
-  console.log('Clicked!');
+  // console.log('Clicked!');
+  navigator.geolocation.getCurrentPosition(
+    (posData) => {
+      setTimeout(() => {
+        console.log(posData);
+      }, 2000);
+    },
+    (errData) => {
+      console.log(errData);
+    }
+  );
+
+  setTimeout(() => {
+    console.log('Timer done! /zero timout/');
+  }, 0);
+
+  console.log('Getting position.../out of navigator/');
 }
 
 button.addEventListener('click', trackUserHandler);
 
 // ----
 
-let result = 0;
+// let result = 0;
 
-for (let i = 0; i < 100000000; i++) {
-  // result = result + i;
-  result += i;
-}
+// for (let i = 0; i < 100000000; i++) {
+//   // result = result + i;
+//   result += i;
+// }
 
-console.log(result);
+// console.log(result);
