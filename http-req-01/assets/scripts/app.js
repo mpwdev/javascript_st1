976f3409173f5af0b00b035556912ceb406e55fd
MP Web Developer <mprwdev@gmail.com>
2024-01-11 19:23:22 +0200

    add create post functionality

--- http-req-01/assets/scripts/app.js
+++ http-req-01/assets/scripts/app.js
@@ -4,7 +4,7 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
@@ -17,7 +17,7 @@ function sendHttpRequest(method, url) {
       // const listOfPosts = JSON.parse(xhr.response);
     };
 
-    xhr.send();
+    xhr.send(JSON.stringify(data));
   });
 
   return promise;
@@ -37,4 +37,16 @@ async function fetchPosts() {
   }
 }
 
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+
+  sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+}
+
 fetchPosts();
+createPost('DUMMY', 'A dummy post text');
