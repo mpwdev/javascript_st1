93769e426ca683437e5b61005b8583963b84a298
MP Web Developer <mprwdev@gmail.com>
2024-01-11 19:15:56 +0200

    promise and Http request

---
+++ http-req-01/assets/scripts/app.js
@@ -0,0 +1,40 @@
+// free demo API
+// https://jsonplaceholder.typicode.com/
+
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+
+    xhr.responseType = 'json';
+
+    xhr.onload = function () {
+      resolve(xhr.response);
+      // const listOfPosts = JSON.parse(xhr.response);
+    };
+
+    xhr.send();
+  });
+
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    'GET',
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+  const listOfPosts = responseData;
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    postEl.querySelector('p').textContent = post.body;
+    listElement.append(postEl);
+  }
+}
+
+fetchPosts();
