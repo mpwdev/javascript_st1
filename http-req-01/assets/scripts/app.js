268c3ae3071128098cd33b21731b408ab835296f
MP Web Developer <mprwdev@gmail.com>
2024-01-12 20:44:23 +0200

    error handling added

--- http-req-01/assets/scripts/app.js
+++ http-req-01/assets/scripts/app.js
@@ -15,9 +15,24 @@ function sendHttpRequest(method, url, data) {
 
     xhr.responseType = 'json';
 
+    // status code errors (non success errors)
     xhr.onload = function () {
-      resolve(xhr.response);
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(
+          new Error('custom message - Something went wrong with HttpRequest')
+        );
+      }
+
       // const listOfPosts = JSON.parse(xhr.response);
+    };
+
+    // network errors
+    xhr.onerror = function () {
+      reject(
+        new Error('custom message - Failed to send request with HttpRequest')
+      );
     };
 
     xhr.send(JSON.stringify(data));
@@ -27,17 +42,21 @@ function sendHttpRequest(method, url, data) {
 }
 
 async function fetchPosts() {
-  const responseData = await sendHttpRequest(
-    'GET',
-    'https://jsonplaceholder.typicode.com/posts'
-  );
-  const listOfPosts = responseData;
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector('h2').textContent = post.title.toUpperCase();
-    postEl.querySelector('p').textContent = post.body;
-    postEl.querySelector('li').id = post.id;
-    listElement.append(postEl);
+  try {
+    const responseData = await sendHttpRequest(
+      'GET',
+      'https://jsonplaceholder.typicode.com/pos'
+    );
+    const listOfPosts = responseData;
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector('h2').textContent = post.title.toUpperCase();
+      postEl.querySelector('p').textContent = post.body;
+      postEl.querySelector('li').id = post.id;
+      listElement.append(postEl);
+    }
+  } catch (error) {
+    alert(error.message);
   }
 }
 
