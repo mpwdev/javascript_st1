990e6e984ee7beb2d6b5138f762a2129cafe3c20
MP Web Developer <mprwdev@gmail.com>
2024-01-13 22:22:15 +0200

    form data with fetch

--- http-req-01/assets/scripts/app_fetch.js
+++ http-req-01/assets/scripts/app_fetch.js
@@ -42,11 +42,10 @@ function sendHttpRequest(method, url, data) {
 
   return fetch(url, {
     method: method,
-    // body: JSON.stringify(data),
-    body: data,
-    // headers: {
-    //   'Content-Type': 'application/json',
-    // },
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
   })
     .then((response) => {
       if (response.status >= 200 && response.status < 300) {
@@ -92,12 +91,7 @@ async function createPost(title, content) {
     userId: userId,
   };
 
-  const fd = new FormData(form);
-  // fd.append('title', title);
-  // fd.append('body', content);
-  fd.append('userId', userId);
-
-  sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', fd);
+  sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
 }
 
 fetchButton.addEventListener('click', fetchPosts);
