6161dda3db297e6480f625f89d5b84164e6eec77
MP Web Developer <mprwdev@gmail.com>
2024-01-17 11:01:33 +0200

    sission storage example

--- storage-01/app.js
+++ storage-01/app.js
@@ -8,18 +8,19 @@ const user = {
   hobbies: ['Programming', 'Sports'],
 };
 
+// sessionStorage (data) cleared after browser restart
 storeBtn.addEventListener('click', () => {
-  localStorage.setItem('uid', userId);
+  sessionStorage.setItem('uid', userId);
   localStorage.setItem('user', JSON.stringify(user));
 });
 
 retrBtn.addEventListener('click', () => {
-  const extractedId = localStorage.getItem('uid');
+  const extractedId = sessionStorage.getItem('uid');
   const extractedUser = JSON.parse(localStorage.getItem('user'));
   console.log(extractedUser);
   if (extractedId) {
-    console.log('Got the id from localStorage - ' + extractedId);
+    console.log('Got the id from sessionStorage - ' + extractedId);
   } else {
-    console.log('Could not find id in localStorage');
+    console.log('Could not find id in sessionStorage');
   }
 });
