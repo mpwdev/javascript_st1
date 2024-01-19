4e3fe341d01e8c18175c3b53a6c4ff40f88ba467
MP Web Developer <mprwdev@gmail.com>
2024-01-19 12:41:24 +0200

    iterators and generators

--- meta-01/app.js
+++ meta-01/app.js
@@ -1,16 +1,31 @@
 const company = {
-  curEmployee: 0,
+  // curEmployee: 0,
   employees: ['Bob', 'Dan', 'Kate'],
-  next() {
-    if (this.curEmployee >= this.employees.length) {
-      return { value: this.curEmployee, done: true };
+  // next() {
+  //   if (this.curEmployee >= this.employees.length) {
+  //     return { value: this.curEmployee, done: true };
+  //   }
+  //   const returnValue = {
+  //     value: this.employees[this.curEmployee],
+  //     done: false,
+  //   };
+  //   this.curEmployee++;
+  //   return returnValue;
+  // },
+  [Symbol.iterator]: function* employeeGenerator() {
+    // let employee = company.next();
+
+    // // we can create own looping logic
+    // while (!employee.done) {
+    //   yield employee.value;
+    //   employee = company.next();
+    // }
+
+    let currentEmployee = 0;
+    while (currentEmployee < this.employees.length) {
+      yield this.employees[currentEmployee];
+			currentEmployee++;
     }
-    const returnValue = {
-      value: this.employees[this.curEmployee],
-      done: false,
-    };
-    this.curEmployee++;
-    return returnValue;
   },
 };
 
@@ -20,10 +35,24 @@ const company = {
 // console.log(company.next());
 // console.log(company.next());
 
-let employee = company.next();
+// let employee = company.next();
 
-// we can create own looping logic
-while (!employee.done) {
-  console.log(employee.value);
-  employee = company.next();
+// // we can create own looping logic
+// while (!employee.done) {
+//   console.log(employee.value);
+//   employee = company.next();
+// }
+
+for (const employee of company) {
+	console.log(employee);
 }
+
+console.log([...company]);
+
+// console.log(company.getEmployee().next());
+
+// const it = company.getEmployee();
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
