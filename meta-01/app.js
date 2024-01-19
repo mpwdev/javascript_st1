8613fa34b1539b6c2ec3f22d553f3af839390112
MP Web Developer <mprwdev@gmail.com>
2024-01-19 12:51:55 +0200

    Reflect API

--- meta-01/app.js
+++ meta-01/app.js
@@ -1,58 +1,14 @@
-const company = {
-  // curEmployee: 0,
-  employees: ['Bob', 'Dan', 'Kate'],
-  // next() {
-  //   if (this.curEmployee >= this.employees.length) {
-  //     return { value: this.curEmployee, done: true };
-  //   }
-  //   const returnValue = {
-  //     value: this.employees[this.curEmployee],
-  //     done: false,
-  //   };
-  //   this.curEmployee++;
-  //   return returnValue;
-  // },
-  [Symbol.iterator]: function* employeeGenerator() {
-    // let employee = company.next();
-
-    // // we can create own looping logic
-    // while (!employee.done) {
-    //   yield employee.value;
-    //   employee = company.next();
-    // }
-
-    let currentEmployee = 0;
-    while (currentEmployee < this.employees.length) {
-      yield this.employees[currentEmployee];
-			currentEmployee++;
-    }
-  },
+const course = {
+  title: 'JavaScript',
 };
 
-// console.log(company.next());
-// console.log(company.next());
-// console.log(company.next());
-// console.log(company.next());
-// console.log(company.next());
+// Reflect API - new
+// Object. API? - older
 
-// let employee = company.next();
+Reflect.setPrototypeOf(course, {
+  toString() {
+    return this.title;
+  },
+});
 
-// // we can create own looping logic
-// while (!employee.done) {
-//   console.log(employee.value);
-//   employee = company.next();
-// }
-
-for (const employee of company) {
-	console.log(employee);
-}
-
-console.log([...company]);
-
-// console.log(company.getEmployee().next());
-
-// const it = company.getEmployee();
-// console.log(it.next());
-// console.log(it.next());
-// console.log(it.next());
-// console.log(it.next());
+console.log(course.toString());
