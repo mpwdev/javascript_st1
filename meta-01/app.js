e5376a04f2965e2278bccb2ad6d2f8bffd3d0e8f
MP Web Developer <mprwdev@gmail.com>
2024-01-19 13:12:04 +0200

    proxy api trap

--- meta-01/app.js
+++ meta-01/app.js
@@ -2,6 +2,7 @@ const course = {
   title: 'JavaScript Title here',
 };
 
+// get and set are Traps in example below
 const courseHandler = {
   get(obj, propertyName) {
     console.log(propertyName);
@@ -11,8 +12,17 @@ const courseHandler = {
     }
     return obj[propertyName] || 'NOT FOUND';
   },
+  set(obj, propertyName, newValue) {
+    console.log('Sending alert to log server ....');
+    if (propertyName === 'rating') {
+      // block access to set
+      return;
+    }
+    obj[propertyName] = newValue;
+  },
 };
 
 const pCourse = new Proxy(course, courseHandler);
+pCourse.rating = 5;
 console.log(pCourse.title, pCourse.length, pCourse.rating);
 // console.log(course, pCourse);
