b0b664298b137a2c55d4f5baa5edaefc3e4d9a54
MP Web Developer <mprwdev@gmail.com>
2024-01-22 14:57:24 +0200

    tooltip styling changes

--- web-components-01/tooltip.js
+++ web-components-01/tooltip.js
@@ -12,11 +12,40 @@ class Tooltip extends HTMLElement {
 			background-color: black;
 			color: white;
 			position: absolute;
+			top: 1.5rem;
+			left: 0.75rem;
 			z-index: 10;
+			padding: 0.3rem;
+			border-radius: 3px;
+			box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.26);
+			font-weight: normal;
+			font-size: 14px;
 		}
+
+		:host(.important) {
+			background-color: var(--color-primary, #ccc);
+			padding: 0.15rem;
+		}
+
+		:host-context(p) {
+			font-weight: bold;
+		}
+
+		::slotted(.highlight) {
+			border-bottom: 2px dotted red;
+		}
+
+		.icon {
+			background-color: gray;
+			color: white;
+			padding: 0.15rem 0.5rem;
+			text-align: center;
+			border-radius: 50%;
+		}
+
 		</style>
 		<slot>Some default slot text</slot>
-		<span> (?)</span>
+		<span class="icon">?</span>
 		`;
   }
 
@@ -31,6 +60,21 @@ class Tooltip extends HTMLElement {
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.shadowRoot.appendChild(tooltipIcon);
     this.style.position = 'relative';
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    // console.log(name, oldValue, newValue);
+    if (oldValue === newValue) {
+      return;
+    }
+
+    if (name === 'text') {
+      this._tooltipText = newValue;
+    }
+  }
+
+  static get observedAttributes() {
+    return ['text'];
   }
 
   _showTooltip() {
