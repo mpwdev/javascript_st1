6d1b54de2cae9492d3321503261a09ffbc718019
MP Web Developer <mprwdev@gmail.com>
2024-01-22 13:50:27 +0200

    web component example 2

--- web-components-01/tooltip.js
+++ web-components-01/tooltip.js
@@ -4,8 +4,20 @@ class Tooltip extends HTMLElement {
     this._tooltipContainer;
     this._tooltipText = 'Some dummy tooltip text';
     this.attachShadow({ mode: 'open' });
-    const template = document.getElementById('tooltip-template');
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    // const template = document.getElementById('tooltip-template');
+    // this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.innerHTML = `
+		<style>
+		div {
+			background-color: black;
+			color: white;
+			position: absolute;
+			z-index: 10;
+		}
+		</style>
+		<slot>Some default slot text</slot>
+		<span> (?)</span>
+		`;
   }
 
   connectedCallback() {
@@ -24,10 +36,10 @@ class Tooltip extends HTMLElement {
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
-    this._tooltipContainer.style.background = 'black';
-    this._tooltipContainer.style.color = 'white';
-    this._tooltipContainer.style.position = 'absolute';
-    this._tooltipContainer.style.zIndex = '10';
+    // this._tooltipContainer.style.background = 'black';
+    // this._tooltipContainer.style.color = 'white';
+    // this._tooltipContainer.style.position = 'absolute';
+    // this._tooltipContainer.style.zIndex = '10';
     this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
