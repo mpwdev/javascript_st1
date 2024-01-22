b54d53d04285102d58c7c3dc4e1cb1c4da3eaaae
MP Web Developer <mprwdev@gmail.com>
2024-01-22 12:52:18 +0200

    use template in web component

--- web-components-01/tooltip.js
+++ web-components-01/tooltip.js
@@ -1,7 +1,38 @@
 class Tooltip extends HTMLElement {
   constructor() {
-		super();
-    console.log('it is working');
+    super();
+    this._tooltipContainer;
+    this._tooltipText = 'Some dummy tooltip text';
+    this.attachShadow({ mode: 'open' });
+    const template = document.getElementById('tooltip-template');
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
+    // const tooltipIcon = document.createElement('span');
+    // tooltipIcon.textContent = ' (?)';
+    const tooltipIcon = this.shadowRoot.querySelector('span');
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = 'relative';
+  }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = this._tooltipText;
+    this._tooltipContainer.style.background = 'black';
+    this._tooltipContainer.style.color = 'white';
+    this._tooltipContainer.style.position = 'absolute';
+    this._tooltipContainer.style.zIndex = '10';
+    this.shadowRoot.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
