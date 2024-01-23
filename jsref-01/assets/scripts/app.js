3fd0d6262d563cf1482cc1fad60060dc2c5ddbc4
MP Web Developer <mprwdev@gmail.com>
2024-01-23 14:12:02 +0200

    js import variants

---
+++ jsref-01/assets/scripts/app.js
@@ -0,0 +1,14 @@
+// import { apiKey } from './util.js'; // .js extension needed
+
+// import apiKey from './util.js';
+// import { abc, data } from './util.js';
+// import { abc, data as content } from './util.js';
+
+// console.log(apiKey);
+// console.log(abc);
+// console.log(data);
+
+// console.log(content);
+
+// import * as util from './util.js';
+// console.log(util.abc);
---
+++ jsref-01/assets/scripts/util.js
@@ -0,0 +1,6 @@
+// export let apiKey = 'asdfareasdare3qwdf1q345';
+
+export default 'asdf786adsf2134asd786234'; // export default only one for this file
+
+export let abc = 'abc-123';
+export let data = 'data-3333';
